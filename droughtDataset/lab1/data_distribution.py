3d9c37226b9f2fe8be331105dca4f033fac51467
MIBismuth <jodulopes@gmail.com>
2022-11-29 10:07:36 +0000

    ds2 granularity final

--- droughtDataset/lab1/data_distribution.py
+++ droughtDataset/lab1/data_distribution.py
@@ -45,7 +45,7 @@ def get_variable_types(data) -> dict:
 variable_types = get_variable_types(data)
 summary5 = data[variable_types['Numeric']].describe()
 # print(summary5)
-data = data[variable_types['Numeric']]
+# data = data[variable_types['Numeric']]
 
 
 
@@ -63,7 +63,7 @@ data = data[variable_types['Numeric']]
 # from matplotlib.pyplot import savefig, show, subplots
 # from ds_charts import get_variable_types, choose_grid, HEIGHT
 #
-# numeric_vars = get_variable_types(data)['Numeric']
+# numeric_vars = get_variable_types(data)['Binary']
 # if [] == numeric_vars:
 #     raise ValueError('There are no numeric variables.')
 # rows, cols = choose_grid(len(numeric_vars))
@@ -73,7 +73,7 @@ data = data[variable_types['Numeric']]
 #     axs[i, j].set_title('Boxplot for %s'%numeric_vars[n])
 #     axs[i, j].boxplot(data[numeric_vars[n]].dropna().values)
 #     i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
-# savefig('images/single_boxplots.png')
+# savefig('images/single_boxplots_binary.png')
 # show()
 #
 #
@@ -110,24 +110,24 @@ data = data[variable_types['Numeric']]
 #
 # HISTOGRAMS
 
-# from matplotlib.pyplot import savefig, show, subplots
-# from ds_charts import get_variable_types, choose_grid, HEIGHT
-#
-# numeric_vars = get_variable_types(data)['Numeric']
-# if [] == numeric_vars:
-#     raise ValueError('There are no numeric variables.')
-#
-# rows, cols = choose_grid(len(numeric_vars))
-# fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
-# i, j = 0, 0
-# for n in range(len(numeric_vars)):
-#     axs[i, j].set_title('Histogram for %s'%numeric_vars[n])
-#     axs[i, j].set_xlabel(numeric_vars[n])
-#     axs[i, j].set_ylabel("nr records")
-#     axs[i, j].hist(data[numeric_vars[n]].dropna().values, 'auto')
-#     i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
-# savefig('images/single_histograms_numeric.png')
-# show()
+from matplotlib.pyplot import savefig, show, subplots
+from ds_charts import get_variable_types, choose_grid, HEIGHT
+
+numeric_vars = get_variable_types(data)['Binary']
+if [] == numeric_vars:
+    raise ValueError('There are no numeric variables.')
+
+rows, cols = choose_grid(len(numeric_vars))
+fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
+i, j = 0, 0
+for n in range(len(numeric_vars)):
+    axs[i, j].set_title('Histogram for %s'%numeric_vars[n])
+    axs[i, j].set_xlabel(numeric_vars[n])
+    axs[i, j].set_ylabel("nr records")
+    axs[i, j].hist(data[numeric_vars[n]].dropna().values, 'auto')
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+savefig('images/single_histograms_numeric_binary.png')
+show()
 
 
 # #DISTRIBUTION SEABORN
@@ -150,45 +150,45 @@ data = data[variable_types['Numeric']]
 #
 #
 #DISTRIBUTION REAL SHIT LETS GO
-from numpy import log
-from pandas import Series
-from scipy.stats import norm, expon, lognorm
-from matplotlib.pyplot import savefig, show, subplots, Axes
-from ds_charts import HEIGHT, multiple_line_chart, get_variable_types
-from ds_charts import get_variable_types, choose_grid, HEIGHT
-numeric_vars = get_variable_types(data)['Numeric']
-rows, cols = choose_grid(len(numeric_vars))
-def compute_known_distributions(x_values: list) -> dict:
-    distributions = dict()
-    # Gaussian
-    mean, sigma = norm.fit(x_values)
-    distributions['Normal(%.1f,%.2f)'%(mean,sigma)] = norm.pdf(x_values, mean, sigma)
-    # Exponential
-    loc, scale = expon.fit(x_values)
-    distributions['Exp(%.2f)'%(1/scale)] = expon.pdf(x_values, loc, scale)
-    # LogNorm
-    sigma, loc, scale = lognorm.fit(x_values)
-    distributions['LogNor(%.1f,%.2f)'%(log(scale),sigma)] = lognorm.pdf(x_values, sigma, loc, scale)
-    return distributions
-
-def histogram_with_distributions(ax: Axes, series: Series, var: str):
-    values = series.sort_values().values
-    ax.hist(values, 20, density=True)
-    distributions = compute_known_distributions(values)
-    multiple_line_chart(values, distributions, ax=ax, title='Best fit for %s'%var)
-
-if [] == numeric_vars:
-    raise ValueError('There are no numeric variables.')
-
-#with the changes
-
-i, j = 0, 0
-for n in range(4, 19):
-    print(n)
-    save = "images/dist/histogram_numeric_distribution" + str(n) + ".png"
-    fig, axs = subplots(1, 1, figsize=(1*HEIGHT, 1*HEIGHT), squeeze=False)
-    histogram_with_distributions(axs[i, j], data[numeric_vars[n]].dropna(), numeric_vars[n])
-    savefig(save)
-# show()
-
+# from numpy import log
+# from pandas import Series
+# from scipy.stats import norm, expon, lognorm
+# from matplotlib.pyplot import savefig, show, subplots, Axes
+# from ds_charts import HEIGHT, multiple_line_chart, get_variable_types
+# from ds_charts import get_variable_types, choose_grid, HEIGHT
+# numeric_vars = get_variable_types(data)['Numeric']
+# rows, cols = choose_grid(len(numeric_vars))
+# def compute_known_distributions(x_values: list) -> dict:
+#     distributions = dict()
+#     # Gaussian
+#     mean, sigma = norm.fit(x_values)
+#     distributions['Normal(%.1f,%.2f)'%(mean,sigma)] = norm.pdf(x_values, mean, sigma)
+#     # Exponential
+#     loc, scale = expon.fit(x_values)
+#     distributions['Exp(%.2f)'%(1/scale)] = expon.pdf(x_values, loc, scale)
+#     # LogNorm
+#     sigma, loc, scale = lognorm.fit(x_values)
+#     distributions['LogNor(%.1f,%.2f)'%(log(scale),sigma)] = lognorm.pdf(x_values, sigma, loc, scale)
+#     return distributions
+#
+# def histogram_with_distributions(ax: Axes, series: Series, var: str):
+#     values = series.sort_values().values
+#     ax.hist(values, 20, density=True)
+#     distributions = compute_known_distributions(values)
+#     multiple_line_chart(values, distributions, ax=ax, title='Best fit for %s'%var)
+#
+# if [] == numeric_vars:
+#     raise ValueError('There are no numeric variables.')
+#
+# #with the changes
+#
+# i, j = 0, 0
+# for n in range(4, 19):
+#     print(n)
+#     save = "images/dist/histogram_numeric_distribution" + str(n) + ".png"
+#     fig, axs = subplots(1, 1, figsize=(1*HEIGHT, 1*HEIGHT), squeeze=False)
+#     histogram_with_distributions(axs[i, j], data[numeric_vars[n]].dropna(), numeric_vars[n])
+#     savefig(save)
+# # show()
+#
 
