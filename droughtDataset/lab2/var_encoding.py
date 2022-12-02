7dec7482f0f554a0ced5c5db55a9592503774b30
MIBismuth <jodulopes@gmail.com>
2022-12-02 13:19:57 +0000

    n/a

--- droughtDataset/lab2/var_encoding.py
+++ droughtDataset/lab2/var_encoding.py
@@ -1,1 +1,12 @@
- 
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+from matplotlib.pyplot import figure, savefig, show
+from ds_charts import bar_chart
+
+register_matplotlib_converters()
+
+# kaggleOGData = pd.read_csv('../kaggleDataset.csv')
+data = pd.read_csv('../drought.csv', na_values='?')
+data['date'] = pd.to_datetime(data['date'], format = '%d/%m/%Y')
+
+#ENCODING DATAS MÊS a MÊS linearmente
