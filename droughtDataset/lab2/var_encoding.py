11439cc829f693cb0ed7511616d5cd0eb8c104e5
MIBismuth <jodulopes@gmail.com>
2022-12-02 13:59:28 +0000

    dates ds2

--- droughtDataset/lab2/var_encoding.py
+++ droughtDataset/lab2/var_encoding.py
@@ -16,3 +16,14 @@ for i in range(len(data["date"])):
 data["date"] = index
 
 data.to_csv('../data/simpledates.csv')
+
+2002/10/14 -> 2002*100 + 10 = 200210
+#ENCODING DATAS CÓDIGO YYYYMM
+
+index = []
+for date in data["date"]:
+    index.append((date.dt.year -2000 )* 100 + date.dt.month)
+
+data["date"] = index
+
+data.to_csv('../data/simpledates.csv')
