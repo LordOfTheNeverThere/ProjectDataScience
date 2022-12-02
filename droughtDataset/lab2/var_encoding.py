062d01183169daae5952f229ff5062ae4c19ef9f
MIBismuth <jodulopes@gmail.com>
2022-12-02 16:06:13 +0000

    some more date type shit

--- droughtDataset/lab2/var_encoding.py
+++ droughtDataset/lab2/var_encoding.py
@@ -2,30 +2,37 @@ import pandas as pd
 from pandas.plotting import register_matplotlib_converters
 from matplotlib.pyplot import figure, savefig, show
 from ds_charts import bar_chart
+import numpy as np
 
 register_matplotlib_converters()
 
 # kaggleOGData = pd.read_csv('../kaggleDataset.csv')
-data = pd.read_csv('../drought.csv', na_values='?')
+data = pd.read_csv('../Data/drought.csv', na_values='?')
 data['date'] = pd.to_datetime(data['date'], format = '%d/%m/%Y')
 
-#ENCODING DATAS LINEAR BÁSICO
-index = []
-for i in range(len(data["date"])):
-    index.append(i)
-data["date"] = index
 
-data.to_csv('../data/simpledates.csv')
+# 2002/10/14 -> 2002*10000 + 10 = 200210
+#ENCODING DATAS CÓDIGO YYYYMMDD
 
-2002/10/14 -> 2002*100 + 10 = 200210
-#ENCODING DATAS CÓDIGO YYYYMM
+# index = []
+# for date in data["date"]:
+#     index.append((date.year -2000 )* 10000 + date.month*100 + date.day)
+#
+# data["date"] = index
+#
+# data.to_csv('../Data/datesyyyymmdd.csv')
+#
+#
+# #EPOCH TIME MUAHAHAHA
+# data = pd.read_csv('../Data/drought.csv', na_values='?')
+# data['date'] = pd.to_datetime(data['date'], format = '%d/%m/%Y')
+# data['date'] = pd.to_datetime(data["date"]).values.astype(np.int64) // 10 ** 6
+# data.to_csv('../Data/datesEPOCH.csv')
 
-index = []
-for date in data["date"]:
-    index.append((date.dt.year -2000 )* 100 + date.dt.month)
 
-data["date"] = index
+#CYCLICAL TYPE BEAT
+data = pd.read_csv('../Data/drought.csv', na_values='?')
+data['date'] = pd.to_datetime(data['date'], format = '%d/%m/%Y')
 
-data.to_csv('../Data/simpledates.csv')
 
-cona
+
