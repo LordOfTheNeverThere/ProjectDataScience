7217ee9e3680aed3ce3900111849ea046fe3f63b
Patricia Marques <mpatricia2002@gmail.com>
2022-12-03 18:21:08 +0000

    outliers treatment so sorry its heaby 1/2 gb

--- droughtDataset/lab2/outliers_treat.py
+++ droughtDataset/lab2/outliers_treat.py
@@ -5,71 +5,94 @@ from ds_charts import get_variable_types
 
 #read the csv
 
-file='drought'
+import os
+os.chdir('../Data/VarEncoding')
+list = os.listdir()
+#list=[]
+list.append('../drought.csv')
 
-register_matplotlib_converters()
-data = read_csv(f'../Data/{file}.csv', na_values='?')
-data.describe()
+for path in list:
+    file=os.path.splitext(path)[0] 
+    print(file)
 
-# Determinate Outliers
+    register_matplotlib_converters()
+    data = read_csv(f'{file}.csv', na_values='?')
+    data.describe()
 
-OUTLIER_PARAM: int = 1.5 # define the number of stdev to use or the IQR scale (usually 1.5)
-OPTION = 'iqr'  # or 'stdev'
+    # Determinate Outliers
 
-def determine_outlier_thresholds(summary5: DataFrame, var: str):
-    if 'iqr' == OPTION:
-        iqr = OUTLIER_PARAM * (summary5[var]['75%'] - summary5[var]['25%'])
-        top_threshold = summary5[var]['75%']  + iqr
-        bottom_threshold = summary5[var]['25%']  - iqr
-    else:  # OPTION == 'stdev'
-        std = OUTLIER_PARAM * summary5[var]['std']
-        top_threshold = summary5[var]['mean'] + std
-        bottom_threshold = summary5[var]['mean'] - std
-    return top_threshold, bottom_threshold
+    OUTLIER_PARAM: int = 1.5 # define the number of stdev to use or the IQR scale (usually 1.5)
+    OPTION = 'stdev'  # or 'stdev'
 
-numeric_vars = get_variable_types(data)['Numeric']
+    def determine_outlier_thresholds(summary5: DataFrame, var: str):
+        if 'iqr' == OPTION:
+            iqr = OUTLIER_PARAM * (summary5[var]['75%'] - summary5[var]['25%'])
+            top_threshold = summary5[var]['75%']  + iqr
+            bottom_threshold = summary5[var]['25%']  - iqr
+        else:  # OPTION == 'stdev'
+            std = OUTLIER_PARAM * summary5[var]['std']
+            top_threshold = summary5[var]['mean'] + std
+            bottom_threshold = summary5[var]['mean'] - std
+        return top_threshold, bottom_threshold
 
-#Dropping Outliers
+    numeric_vars = get_variable_types(data)['Numeric']
 
-if [] == numeric_vars:
-    raise ValueError('There are no numeric variables.')
-print('Original data:', data.shape)
+    #Dropping Outliers
 
-summary5 = data.describe(include='number')
-df = data.copy(deep=True)
-for var in numeric_vars:
-    top_threshold, bottom_threshold = determine_outlier_thresholds(summary5, var)
-    outliers = df[(df[var] > top_threshold) | (df[var] < bottom_threshold)]
-    df.drop(outliers.index, axis=0, inplace=True)
+    if [] == numeric_vars:
+        raise ValueError('There are no numeric variables.')
+    print('Original data:', data.shape)
 
-df.to_csv(f'../Data/OutliersTreat/{file}_drop_outliers.csv', index=True)
-print('data after dropping outliers:', df.shape)
+    summary5 = data.describe(include='number')
+    df = data.copy(deep=True)
+    for var in numeric_vars:
+        top_threshold, bottom_threshold = determine_outlier_thresholds(summary5, var)
+        outliers = df[(df[var] > top_threshold) | (df[var] < bottom_threshold)]
+        df.drop(outliers.index, axis=0, inplace=True)
 
-#Replacing outliers with fixed value (in this case, median value)
+    df.to_csv(f'../OutliersTreat/{file}_{OPTION}_drop_outliers.csv', index=True)
+    print('data after dropping outliers:', df.shape)
 
-if [] == numeric_vars:
-    raise ValueError('There are no numeric variables.')
+    #Replacing outliers with fixed value (in this case, median value)
 
-summary5 = data.describe(include='number')
-df = data.copy(deep=True)
-for var in numeric_vars:
-    top_threshold, bottom_threshold = determine_outlier_thresholds(summary5, var)
-    median = df[var].median()
-    df[var] = df[var].apply(lambda x: median if x > top_threshold or x < bottom_threshold else x)
+    if [] == numeric_vars:
+        raise ValueError('There are no numeric variables.')
 
-print('data after replacing outliers:', df.describe())
-df.to_csv(f'../Data/OutliersTreat/{file}_replacing_outliers.csv', index=True)
+    summary5 = data.describe(include='number')
+    df = data.copy(deep=True)
+    for var in numeric_vars:
+        top_threshold, bottom_threshold = determine_outlier_thresholds(summary5, var)
+        median = df[var].median()
+        df[var] = df[var].apply(lambda x: median if x > top_threshold or x < bottom_threshold else x)
 
-# Truncating outliers
+    print('data after replacing outliers:', df.describe())
+    df.to_csv(f'../OutliersTreat/{file}_{OPTION}_replacing_outliers_median.csv', index=True)
 
-if [] == numeric_vars:
-    raise ValueError('There are no numeric variables.')
+    #Replacing outliers with fixed value (in this case, mean value)
 
-summary5 = data.describe(include='number')
-df = data.copy(deep=True)
-for var in numeric_vars:
-    top_threshold, bottom_threshold = determine_outlier_thresholds(summary5, var)
-    df[var] = df[var].apply(lambda x: top_threshold if x > top_threshold else bottom_threshold if x < bottom_threshold else x)
+    if [] == numeric_vars:
+        raise ValueError('There are no numeric variables.')
 
-print('data after truncating outliers:', df.describe())
-df.to_csv(f'../Data/OutliersTreat/{file}_truncate_outliers.csv', index=True)
+    summary5 = data.describe(include='number')
+    df = data.copy(deep=True)
+    for var in numeric_vars:
+        top_threshold, bottom_threshold = determine_outlier_thresholds(summary5, var)
+        median = df[var].mean()
+        df[var] = df[var].apply(lambda x: median if x > top_threshold or x < bottom_threshold else x)
+
+    print('data after replacing outliers:', df.describe())
+    df.to_csv(f'../OutliersTreat/{file}_{OPTION}_replacing_outliers_mean.csv', index=True)
+
+    # Truncating outliers
+
+    if [] == numeric_vars:
+        raise ValueError('There are no numeric variables.')
+
+    summary5 = data.describe(include='number')
+    df = data.copy(deep=True)
+    for var in numeric_vars:
+        top_threshold, bottom_threshold = determine_outlier_thresholds(summary5, var)
+        df[var] = df[var].apply(lambda x: top_threshold if x > top_threshold else bottom_threshold if x < bottom_threshold else x)
+
+    print('data after truncating outliers:', df.describe())
+    df.to_csv(f'../OutliersTreat/{file}_{OPTION}_truncate_outliers.csv', index=True)
