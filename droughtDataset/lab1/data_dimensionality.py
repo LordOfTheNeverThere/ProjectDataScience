91ce83b0bfea79b69aa4306268887de09af93058
MIBismuth <jodulopes@gmail.com>
2022-12-05 23:04:46 +0000

    cleaning house

--- droughtDataset/lab1/data_dimensionality.py
+++ droughtDataset/lab1/data_dimensionality.py
@@ -8,7 +8,7 @@ from ds_charts import bar_chart
 register_matplotlib_converters()
 
 # kaggleOGData = pd.read_csv('../kaggleDataset.csv')
-data = pd.read_csv('../drought.csv', na_values='?')
+data = pd.read_csv('../Data/drought.csv', na_values='-1')
 data['date'] = pd.to_datetime(data['date'], format = '%d/%m/%Y')
 data.shape
 
