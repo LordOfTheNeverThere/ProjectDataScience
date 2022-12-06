2d143f14db77b0d2fcffa4b585d8bab82b9d2b0a
MIBismuth <jodulopes@gmail.com>
2022-12-06 01:28:17 +0000

    bayes e knn ds2

--- droughtDataset/lab2/bayesknn.py
+++ droughtDataset/lab2/bayesknn.py
@@ -8,10 +8,12 @@ from sklearn.model_selection import train_test_split
 
 
 import os
-os.chdir('../Data/OutliersTreat')
+os.chdir('../Data/Scaled')
 
-file_tag = 'drought'
-data: DataFrame = read_csv('datesCyclical_stdev_drop_outliers.csv')
+
+#PARA MINMAX
+file_tag = 'datesCyclical_stdev_drop_outliers_scaled_minmax'
+data: DataFrame = read_csv('datesCyclical_stdev_drop_outliers_scaled_minmax.csv')
 target = 'class'
 positive = 1
 negative = 0
@@ -36,3 +38,246 @@ plt.figure(figsize=(12,4))
 ds.multiple_bar_chart([positive, negative], values, title='Data distribution per dataset')
 plt.show()
 
+from numpy import ndarray
+from pandas import DataFrame, read_csv, unique
+from matplotlib.pyplot import figure, savefig, show
+from sklearn.naive_bayes import GaussianNB
+from ds_charts import plot_evaluation_results, bar_chart
+
+file_tag = 'drought'
+filename = 'datesCyclical_stdev_drop_outliers_scaled_minmax'
+target = 'class'
+
+train: DataFrame = read_csv(f'../TrainTest/{filename}_train.csv')
+trnY: ndarray = train.pop(target).values
+trnX: ndarray = train.values
+labels = unique(trnY)
+labels.sort()
+
+test: DataFrame = read_csv(f'../TrainTest/{filename}_test.csv')
+tstY: ndarray = test.pop(target).values
+tstX: ndarray = test.values
+
+clf = GaussianNB()
+clf.fit(trnX, trnY)
+prd_trn = clf.predict(trnX)
+prd_tst = clf.predict(tstX)
+plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
+savefig(f'../../lab2/images/NB/{file_tag}_nb_best_minmax.png')
+show()
+
+
+
+
+
+
+
+
+
+
+# #PARA ZSCORE
+file_tag = 'datesCyclical_stdev_drop_outliers_scaled_zscore'
+data: DataFrame = read_csv('datesCyclical_stdev_drop_outliers_scaled_zscore.csv')
+target = 'class'
+positive = 1
+negative = 0
+values = {'Original': [len(data[data[target] == positive]), len(data[data[target] == negative])]}
+
+y: np.ndarray = data.pop(target).values
+X: np.ndarray = data.values
+labels: np.ndarray = unique(y)
+labels.sort()
+
+trnX, tstX, trnY, tstY = train_test_split(X, y, train_size=0.7, stratify=y)
+
+train = concat([DataFrame(trnX, columns=data.columns), DataFrame(trnY,columns=[target])], axis=1)
+train.to_csv(f'../TrainTest/{file_tag}_train.csv', index=False)
+
+test = concat([DataFrame(tstX, columns=data.columns), DataFrame(tstY,columns=[target])], axis=1)
+test.to_csv(f'../TrainTest/{file_tag}_test.csv', index=False)
+values['Train'] = [len(np.delete(trnY, np.argwhere(trnY==negative))), len(np.delete(trnY, np.argwhere(trnY==positive)))]
+values['Test'] = [len(np.delete(tstY, np.argwhere(tstY==negative))), len(np.delete(tstY, np.argwhere(tstY==positive)))]
+
+plt.figure(figsize=(12,4))
+ds.multiple_bar_chart([positive, negative], values, title='Data distribution per dataset')
+plt.show()
+
+from numpy import ndarray
+from pandas import DataFrame, read_csv, unique
+from matplotlib.pyplot import figure, savefig, show
+from sklearn.naive_bayes import GaussianNB
+from ds_charts import plot_evaluation_results, bar_chart
+
+file_tag = 'drought'
+filename = 'datesCyclical_stdev_drop_outliers_scaled_zscore'
+target = 'class'
+
+train: DataFrame = read_csv(f'../TrainTest/{filename}_train.csv')
+trnY: ndarray = train.pop(target).values
+trnX: ndarray = train.values
+labels = unique(trnY)
+labels.sort()
+
+test: DataFrame = read_csv(f'../TrainTest/{filename}_test.csv')
+tstY: ndarray = test.pop(target).values
+tstX: ndarray = test.values
+
+clf = GaussianNB()
+clf.fit(trnX, trnY)
+prd_trn = clf.predict(trnX)
+prd_tst = clf.predict(tstX)
+plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
+savefig(f'../../lab2/images/NB/{file_tag}_nb_best_zscore.png')
+show()
+
+
+
+
+#KNN*********************************************************
+#***********************************************************
+
+#MinnnnnnnnnnnMAAAAAAAAAAAAAAAX
+from numpy import ndarray
+from pandas import DataFrame, read_csv, unique
+from matplotlib.pyplot import figure, savefig, show
+from sklearn.neighbors import KNeighborsClassifier
+from ds_charts import plot_evaluation_results, multiple_line_chart, plot_overfitting_study
+from sklearn.metrics import accuracy_score
+
+file_tag = 'drought'
+filename = 'datesCyclical_stdev_drop_outliers_scaled_minmax'
+target = 'class'
+
+
+train: DataFrame = read_csv(f'../TrainTest/{filename}_train.csv')
+trnY: ndarray = train.pop(target).values
+trnX: ndarray = train.values
+labels = unique(trnY)
+labels.sort()
+
+test: DataFrame = read_csv(f'../TrainTest/{filename}_test.csv')
+tstY: ndarray = test.pop(target).values
+tstX: ndarray = test.values
+
+eval_metric = accuracy_score
+nvalues = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+dist = ['manhattan', 'euclidean', 'chebyshev']
+values = {}
+best = (0, '')
+last_best = 0
+for d in dist:
+    y_tst_values = []
+    for n in nvalues:
+        knn = KNeighborsClassifier(n_neighbors=n, metric=d)
+        knn.fit(trnX, trnY)
+        prd_tst_Y = knn.predict(tstX)
+        y_tst_values.append(eval_metric(tstY, prd_tst_Y))
+        if y_tst_values[-1] > last_best:
+            best = (n, d)
+            last_best = y_tst_values[-1]
+    values[d] = y_tst_values
+
+figure()
+multiple_line_chart(nvalues, values, title='KNN variants', xlabel='n', ylabel=str(accuracy_score), percentage=True)
+savefig('../../lab2/images/KNN/{file_tag}_knn_study_minmax.png')
+show()
+print('Best results with %d neighbors and %s'%(best[0], best[1]))
+
+clf = knn = KNeighborsClassifier(n_neighbors=best[0], metric=best[1])
+clf.fit(trnX, trnY)
+prd_trn = clf.predict(trnX)
+prd_tst = clf.predict(tstX)
+plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
+savefig('../../lab2/images/KNN/{file_tag}_knn_best_minmax.png')
+show()
+
+from matplotlib.pyplot import figure, savefig
+
+def plot_overfitting_study(xvalues, prd_trn, prd_tst, name, xlabel, ylabel):
+    evals = {'Train': prd_trn, 'Test': prd_tst}
+    figure()
+    multiple_line_chart(xvalues, evals, ax = None, title=f'Overfitting {name}', xlabel=xlabel, ylabel=ylabel, percentage=True)
+    savefig('../../lab2/images/KNN/overfitting_{name}_minmax.png')
+
+d = 'euclidean'
+eval_metric = accuracy_score
+y_tst_values = []
+y_trn_values = []
+for n in nvalues:
+    knn = KNeighborsClassifier(n_neighbors=n, metric=d)
+    knn.fit(trnX, trnY)
+    prd_tst_Y = knn.predict(tstX)
+    prd_trn_Y = knn.predict(trnX)
+    y_tst_values.append(eval_metric(tstY, prd_tst_Y))
+    y_trn_values.append(eval_metric(trnY, prd_trn_Y))
+plot_overfitting_study(nvalues, y_trn_values, y_tst_values, name=f'KNN_K={n}_{d}', xlabel='K', ylabel=str(eval_metric))
+
+
+#ZSCOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOORE
+
+file_tag = 'drought'
+filename = 'datesCyclical_stdev_drop_outliers_scaled_zscore'
+target = 'class'
+
+train: DataFrame = read_csv(f'../TrainTest/{filename}_train.csv')
+trnY: ndarray = train.pop(target).values
+trnX: ndarray = train.values
+labels = unique(trnY)
+labels.sort()
+
+test: DataFrame = read_csv(f'../TrainTest/{filename}_test.csv')
+tstY: ndarray = test.pop(target).values
+tstX: ndarray = test.values
+
+eval_metric = accuracy_score
+nvalues = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+dist = ['manhattan', 'euclidean', 'chebyshev']
+values = {}
+best = (0, '')
+last_best = 0
+for d in dist:
+    y_tst_values = []
+    for n in nvalues:
+        knn = KNeighborsClassifier(n_neighbors=n, metric=d)
+        knn.fit(trnX, trnY)
+        prd_tst_Y = knn.predict(tstX)
+        y_tst_values.append(eval_metric(tstY, prd_tst_Y))
+        if y_tst_values[-1] > last_best:
+            best = (n, d)
+            last_best = y_tst_values[-1]
+    values[d] = y_tst_values
+
+figure()
+multiple_line_chart(nvalues, values, title='KNN variants', xlabel='n', ylabel=str(accuracy_score), percentage=True)
+savefig('../../lab2/images/KNN/{file_tag}_knn_zscore.png')
+show()
+print('Best results with %d neighbors and %s'%(best[0], best[1]))
+
+clf = knn = KNeighborsClassifier(n_neighbors=best[0], metric=best[1])
+clf.fit(trnX, trnY)
+prd_trn = clf.predict(trnX)
+prd_tst = clf.predict(tstX)
+plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
+savefig('../../lab2/images/KNN/{file_tag}_knn_best_zscore.png')
+show()
+
+from matplotlib.pyplot import figure, savefig
+
+def plot_overfitting_study(xvalues, prd_trn, prd_tst, name, xlabel, ylabel):
+    evals = {'Train': prd_trn, 'Test': prd_tst}
+    figure()
+    multiple_line_chart(xvalues, evals, ax = None, title=f'Overfitting {name}', xlabel=xlabel, ylabel=ylabel, percentage=True)
+    savefig('../../lab2/images/KNN/overfitting_{name}.png')
+
+d = 'euclidean'
+eval_metric = accuracy_score
+y_tst_values = []
+y_trn_values = []
+for n in nvalues:
+    knn = KNeighborsClassifier(n_neighbors=n, metric=d)
+    knn.fit(trnX, trnY)
+    prd_tst_Y = knn.predict(tstX)
+    prd_trn_Y = knn.predict(trnX)
+    y_tst_values.append(eval_metric(tstY, prd_tst_Y))
+    y_trn_values.append(eval_metric(trnY, prd_trn_Y))
+plot_overfitting_study(nvalues, y_trn_values, y_tst_values, name=f'KNN_K={n}_{d}', xlabel='K', ylabel=str(eval_metric))
