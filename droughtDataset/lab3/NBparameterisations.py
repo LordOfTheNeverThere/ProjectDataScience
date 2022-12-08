734b7f88fc0552cafaedab48778e3b7d9ac0b44c
Patricia Marques <mpatricia2002@gmail.com>
2022-12-08 12:59:44 +0000

    set2 nb knn

--- droughtDataset/lab3/NBparameterisations.py
+++ droughtDataset/lab3/NBparameterisations.py
@@ -7,7 +7,7 @@ import ds_charts as ds
 from sklearn.model_selection import train_test_split
 
 file_tag = 'drought_prepared'
-data: DataFrame = read_csv(f'Data/{file_tag}.csv')
+data: DataFrame = read_csv(f'../Data/{file_tag}.csv')
 target = 'class'
 positive = 1
 negative = 0
@@ -23,10 +23,10 @@ labels.sort()
 trnX, tstX, trnY, tstY = train_test_split(X, y, train_size=0.7, stratify=y)
 
 train = concat([DataFrame(trnX, columns=data.columns), DataFrame(trnY,columns=[target])], axis=1)
-train.to_csv(f'Data/TrainTest/{file_tag}_train.csv', index=False)
+train.to_csv(f'../Data/TrainTest/{file_tag}_train.csv', index=False)
 
 test = concat([DataFrame(tstX, columns=data.columns), DataFrame(tstY,columns=[target])], axis=1)
-test.to_csv(f'Data/TrainTest/{file_tag}_test.csv', index=False)
+test.to_csv(f'../Data/TrainTest/{file_tag}_test.csv', index=False)
 values['Train'] = [len(np.delete(trnY, np.argwhere(trnY==negative))), len(np.delete(trnY, np.argwhere(trnY==positive)))]
 values['Test'] = [len(np.delete(tstY, np.argwhere(tstY==negative))), len(np.delete(tstY, np.argwhere(tstY==positive)))]
 
@@ -65,13 +65,13 @@ file_tag = 'drought_prepared'
 filename = 'drought_prepared'
 target = 'class'
 
-train: DataFrame = read_csv(f'../TrainTest/{filename}_train.csv')
+train: DataFrame = read_csv(f'../Data/TrainTest/{filename}_train.csv')
 trnY: ndarray = train.pop(target).values
 trnX: ndarray = train.values
 labels = unique(trnY)
 labels.sort()
 
-test: DataFrame = read_csv(f'../TrainTest/{filename}_test.csv')
+test: DataFrame = read_csv(f'../Data/TrainTest/{filename}_test.csv')
 tstY: ndarray = test.pop(target).values
 tstX: ndarray = test.values
 
@@ -80,6 +80,29 @@ clf.fit(trnX, trnY)
 prd_trn = clf.predict(trnX)
 prd_tst = clf.predict(tstX)
 plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
-savefig(f'../lab3/images/NB/{file_tag}_nb_best.png')
+savefig(f'images/NB/{file_tag}_nb_best.png')
 show()
 
+# accurady of different parameterisations
+
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB, CategoricalNB
+from sklearn.metrics import accuracy_score
+
+estimators = {'GaussianNB': GaussianNB(),
+              'MultinomialNB': MultinomialNB(),
+              'BernoulliNB': BernoulliNB()
+              #'CategoricalNB': CategoricalNB
+              }
+
+xvalues = []
+yvalues = []
+for clf in estimators:
+    xvalues.append(clf)
+    estimators[clf].fit(trnX, trnY)
+    prdY = estimators[clf].predict(tstX)
+    yvalues.append(accuracy_score(tstY, prdY))
+
+figure()
+bar_chart(xvalues, yvalues, title='Comparison of Naive Bayes Models', ylabel='accuracy', percentage=True)
+savefig(f'images/{file_tag}_nb_study.png')
+show()
