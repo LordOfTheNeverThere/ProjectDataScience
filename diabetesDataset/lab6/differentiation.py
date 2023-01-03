1ac024231e25e506cb825927058b2609b3346d6e
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-01-03 23:12:11 +0000

    Differentiation Finished

--- diabetesDataset/lab6/differentiation.py
+++ diabetesDataset/lab6/differentiation.py
@@ -4,11 +4,24 @@ from matplotlib.pyplot import figure, xticks, show
 from ts_functions import plot_series, HEIGHT
 
 # %% Get Glucose Data read_csv
-data = Dataframe()
-# %% Differentiation  The role of this technique is to adress non stationary data, removing the influence of the trend I think
-data = data.diff()
+data = read_csv()
+
+
+# %% 1st Differentiation  The role of this technique is to adress non stationary data, removing the influence of the trend I think
+
+dataDiff1 = data.diff()
 figure(figsize=(3*HEIGHT, HEIGHT))
 plot_series(data['Glucose'], title='Diabetes - Differentiation', y_label='Glucose')
+plot_series(data['Insulin'], y_label='Insulin')
+xticks(rotation=45)
+show()
+
+# %% 2nd Differentiation  The role of this technique is to adress non stationary data, removing the influence of the trend I think
+
+dataDiff2 = dataDiff1.diff()
+figure(figsize=(3*HEIGHT, HEIGHT))
+plot_series(data['Glucose'],
+            title='Diabetes - Differentiation', y_label='Glucose')
 plot_series(data['Insulin'], y_label='Insulin')
 xticks(rotation=45)
 show()
